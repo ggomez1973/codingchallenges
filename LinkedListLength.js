56f219f800af009477aa7427a09272c5bb9875ef
ggomez1973 <ggomez1973@gmail.com>
2014-08-19 16:11:45 -0300

    Linked List Length test Codility
    Get the number of nodes in a list.

---
+++ LinkedListLength.js
@@ -0,0 +1,22 @@
+// Linked List Length
+
+var LL = {
+	value : 'A',
+	next : {
+		value : 'B',
+		next : {
+			value : 'C',
+			next : null
+		}	
+	}
+}
+
+function counter(List){
+	if(!List){
+		return 0;
+	} else {
+		return 1 + counter(List.next);
+	}
+}
+
+console.log(counter(LL));
